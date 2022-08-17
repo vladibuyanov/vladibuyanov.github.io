eb76a353bca2101542e88a55324578c65381a928
vladibuyanov <vladibuyanov@gmail.com>
2022-08-17 11:26:01 +0200

    Ver. 17/08/2022

--- js/theme.js
+++ js/theme.js
@@ -2,12 +2,16 @@ function setTheme() {
     let body = document.querySelector("body");
     let links_img = document.getElementsByClassName('title-link');
     let github_link_img = document.getElementById('github_link_img');
+    let car_btn = document.getElementsByClassName('btn');
     if (theme_btn.value == 'light') {
         body.style.background = 'white';
         body.style.color = 'black';
         github_link_img.src = 'img/github.png';
         for (let i = 0; i < links_img.length; i++) {
             links_img[i].src = "img/link.png";
+        }
+        for (let i = 0; i < car_btn.length; i++) {
+            car_btn[i].style.color = 'black';
         }
     }
     else {
@@ -16,6 +20,9 @@ function setTheme() {
         github_link_img.src = 'img/github_dark.png';
         for (let i = 0; i < links_img.length; i++) {
             links_img[i].src = "img/link_dark.png";
+        }
+        for (let i = 0; i < car_btn.length; i++) {
+            car_btn[i].style.color = 'white';
         }
     }
 }
