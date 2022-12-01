3b44bc97a447d73b9a1dc87dff4c62bab411a0c2
vladibuyanov <vladibuyanov@gmail.com>
2022-12-01 16:10:37 +0100

    Ver. 01/12/2022

--- js/theme.js
+++ js/theme.js
@@ -1,12 +1,20 @@
+
 function setTheme() {
     let body = document.querySelector("body");
+    let links = [document.querySelectorAll("a")[6], document.querySelectorAll("a")[7]];
     let links_img = document.getElementsByClassName('title-link');
     let github_link_img = document.getElementById('github_link_img');
     let car_btn = document.getElementsByClassName('btn');
+
     if (theme_btn.value == 'light') {
         body.style.background = 'white';
         body.style.color = 'black';
         github_link_img.src = 'img/github.png';
+
+        for (let i = 0; i < links.length; i++) {
+            console.log(links[i])
+            links[i].style.color = 'black';
+        }
         for (let i = 0; i < links_img.length; i++) {
             links_img[i].src = "img/link.png";
         }
@@ -18,6 +26,11 @@ function setTheme() {
         body.style.background = 'black';
         body.style.color = 'white';
         github_link_img.src = 'img/github_dark.png';
+
+        for (let i = 0; i < links.length; i++) {
+            console.log(links[i])
+            links[i].style.color = 'white';
+        }
         for (let i = 0; i < links_img.length; i++) {
             links_img[i].src = "img/link_dark.png";
         }
