2113c83ca8827930aa3f796f8a5f19ba00d55afd
vladibuyanov <vladibuyanov@gmail.com>
2022-12-06 18:46:56 +0100

    Ver. 06/12/2022

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,12 @@
-a = [document.querySelectorAll("a")[9], document.querySelectorAll("a")[10]]
-console.log(a)
-
 function setTheme() {
     let body = document.querySelector("body");
-    let links = [document.querySelectorAll("a")[9], document.querySelectorAll("a")[10]];
+    let links = [
+            document.querySelectorAll("a")[6],
+            document.querySelectorAll("a")[7],
+            document.querySelectorAll("a")[8],
+            document.querySelectorAll("a")[9],
+            document.querySelectorAll("a")[10]
+        ];
     let links_img = document.getElementsByClassName('title-link');
     let github_link_img = document.getElementById('github_link_img');
     let car_btn = document.getElementsByClassName('btn');
@@ -13,11 +16,7 @@ function setTheme() {
         body.style.color = 'black';
 
         for (let i = 0; i < links.length; i++) {
-            console.log(links[i])
             links[i].style.color = 'black';
-        }
-        for (let i = 0; i < links_img.length; i++) {
-            links_img[i].src = "img/link.svg";
         }
         for (let i = 0; i < car_btn.length; i++) {
             car_btn[i].style.color = 'black';
@@ -28,11 +27,7 @@ function setTheme() {
         body.style.color = 'white';
 
         for (let i = 0; i < links.length; i++) {
-            console.log(links[i])
             links[i].style.color = 'white';
-        }
-        for (let i = 0; i < links_img.length; i++) {
-            links_img[i].src = "img/link_dark.svg";
         }
         for (let i = 0; i < car_btn.length; i++) {
             car_btn[i].style.color = 'white';
