448a1bb51d9e0bf490492668524eda45ad6d1561
Vladi <vladibuyanov@gmail.com>
2023-02-26 12:47:07 +0100

    26/02/23
    Small Fix

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # Portfolio Website Vladibuyanov | [link](https://vladibuyanov.github.io/)
 
-
 This is a static portfolio website built with HTML, CSS and JS. 
-It showcases my work and skills as a web developer.
+It showcases my work and skills developer.
 
 ## Getting Started
 To view the website, simply clone or download the repository and open the index.html file in your browser.
-
-## Features
-- Responsive design for optimal viewing on all devices
-- Clean and modern layout
-- Easy navigation to different sections of the website
 
 ## Built With
 - HTML
--- js/theme.js
+++ js/theme.js
@@ -1,37 +1,57 @@
 function setTheme() {
     let body = document.querySelector("body");
-    let links = [
-            document.querySelectorAll("a")[6],
-            document.querySelectorAll("a")[7],
-            document.querySelectorAll("a")[8],
-            document.querySelectorAll("a")[9],
-            document.querySelectorAll("a")[10]
-        ];
-    let links_img = document.getElementsByClassName('title-link');
-    let github_link_img = document.getElementById('github_link_img');
     let car_btn = document.getElementsByClassName('btn');
+    let second_color = document.getElementsByClassName('second-color');
+
+    // Получить элементы img с классом "img-link"
+    let imgLinks = document.querySelectorAll(".footer-img");
+    let imgTitle = document.querySelectorAll(".title-img");
+
 
     if (theme_btn.value == 'light') {
         body.style.background = 'white';
         body.style.color = 'black';
 
-        for (let i = 0; i < links.length; i++) {
-            links[i].style.color = 'black';
-        }
         for (let i = 0; i < car_btn.length; i++) {
             car_btn[i].style.color = 'black';
         }
+        for (let i = 0; i < second_color.length; i++) {
+            second_color[i].style.color = 'black';
+        }
+        // Пройти по всем элементам img и изменить их атрибуты src
+        imgLinks.forEach(function(link) {
+          const fileName = link.getAttribute("src").split("/").pop();
+          const newSrc = `img/social-links/${fileName}`;
+          link.setAttribute("src", newSrc);
+        });
+        imgTitle.forEach(function(link) {
+          const fileName = link.getAttribute("src").split("/").pop();
+          const newSrc = `img/buttons/${fileName}`;
+          link.setAttribute("src", newSrc);
+        });
+
     }
     else {
         body.style.background = 'black';
         body.style.color = 'white';
 
-        for (let i = 0; i < links.length; i++) {
-            links[i].style.color = 'white';
-        }
         for (let i = 0; i < car_btn.length; i++) {
             car_btn[i].style.color = 'white';
         }
+        for (let i = 0; i < second_color.length; i++) {
+            second_color[i].style.color = 'white';
+        }
+        // Пройти по всем элементам img и изменить их атрибуты src
+        imgLinks.forEach(function(link) {
+          const fileName = link.getAttribute("src").split("/").pop();
+          const newSrc = `img/social-links/white/${fileName}`;
+          link.setAttribute("src", newSrc);
+        });
+        imgTitle.forEach(function(link) {
+          const fileName = link.getAttribute("src").split("/").pop();
+          const newSrc = `img/buttons/white/${fileName}`;
+          link.setAttribute("src", newSrc);
+        });
     }
 }
 
