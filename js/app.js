27a05fa80b9d64b4f9fcb02d8f8f755b1c5e3e5c
Vladi <vladibuyanov@gmail.com>
2024-02-12 00:54:58 +0100

    Был исправлен баг, в результате которого, иконки при переключении в темном цвете не отображались.
    Closes #9

--- js/app.js
+++ js/app.js
@@ -1,43 +1,4 @@
-// Функция для загрузки и вставки контента
-function loadAndInsertContent(lang) {
-    return new Promise((resolve, reject) => {
-        let filesToLoad;
-        if (lang === enLangId) {
-            filesToLoad = [headerFile_en, mainFile_en, footerFile_en];
-        } else if (lang === ruLangId) {
-            filesToLoad = [headerFile_ru, mainFile_ru, footerFile_ru];
-        } else {
-            filesToLoad = [headerFile_sk, mainFile_sk, footerFile_sk];
-        }
-        const containers = [headerContainer, mainContainer, footerContainer];
-
-        // Счетчик для отслеживания успешно загруженных файлов
-        let loadedFilesCount = 0;
-
-        filesToLoad.forEach((file, index) => {
-            fetch(file)
-                .then(response => response.text())
-                .then(data => {
-                    containers[index].innerHTML = data;
-                    loadedFilesCount++;
-
-                    // Проверяем, все ли файлы были успешно загружены
-                    if (loadedFilesCount === filesToLoad.length) {
-                        resolve();
-                    }
-                })
-                .catch(error => {
-                    console.error("Ошибка при загрузке контента: " + error);
-                    reject(error);
-                });
-        });
-    });
-}
-
-const headerContainer = document.getElementById("header-content");
-const mainContainer = document.getElementById("main");
-const footerContainer = document.getElementById("footer");
-
+/* Constants */
 const headerFile_en = "./html/en/header.html";
 const mainFile_en = "./html/en/main.html";
 const footerFile_en = "./html/en/footer.html";
@@ -54,10 +15,79 @@ let enLangId = 'lang-en';
 let ruLangId = 'lang-ru';
 let skLangId = 'lang-sk';
 
-// Вызываем первую функцию и после ее завершения вызываем вторую
+let langFiles = {
+    'lang-en': [headerFile_en, mainFile_en, footerFile_en],
+    'lang-ru': [headerFile_ru, mainFile_ru, footerFile_ru],
+    'lang-sk': [headerFile_sk, mainFile_sk, footerFile_sk]
+};
+
+/**/
+function set_color(elements, color) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.color = color;
+    }
+}
+
+/* Functions */
+function loadAndInsertContent(lang) {
+    return new Promise((resolve, reject) => {
+        filesToLoad = langFiles[lang];
+        const containers = [headerContainer, mainContainer, footerContainer];
+        let loadedFilesCount = 0;
+
+        filesToLoad.forEach((file, index) => {
+            fetch(file)
+                .then(response => response.text())
+                .then(data => {
+                    containers[index].innerHTML = data;
+                    loadedFilesCount++;
+
+                    let theme_btn = document.getElementById('theme');
+                    if (theme_btn.value == 'dark') {
+                        let imgLinks = document.querySelectorAll(".footer-img");
+                        let imgTitle = document.querySelectorAll(".title-img");
+                        let page_links = document.getElementsByClassName('page-links');
+
+                        updateIconColors('white');
+                        set_color(page_links, 'white');
+                    };
+
+                    if (loadedFilesCount === filesToLoad.length) {
+                        resolve();
+                    }
+                })
+                .catch(error => {
+                    console.error("Ошибка при загрузке контента: " + error);
+                    reject(error);
+                });
+        });
+
+
+    });
+}
+
+function updateIconColors(color) {
+    let imgLinks = document.querySelectorAll(".footer-img");
+    let imgTitle = document.querySelectorAll(".title-img");
+
+    imgLinks.forEach(function(link) {
+        const fileName = link.getAttribute("src").split("/").pop();
+        const newSrc = `img/social-links/${color}/${fileName}`;
+        link.setAttribute("src", newSrc);
+    });
+    imgTitle.forEach(function(link) {
+        const fileName = link.getAttribute("src").split("/").pop();
+        const newSrc = `img/buttons/${color}/${fileName}`;
+        link.setAttribute("src", newSrc);
+    });
+}
+
+const headerContainer = document.getElementById("header-content");
+const mainContainer = document.getElementById("main");
+const footerContainer = document.getElementById("footer");
+
 loadAndInsertContent(enLangId)
     .then(() => {
-        // Второй скрипт начинается здесь
         let currentIndex = 0;
         let items = document.getElementsByClassName('carousel_item');
         let totalItems = items.length;
@@ -78,6 +108,10 @@ loadAndInsertContent(enLangId)
     });
 
 addEventListener('load', function () {
+    function setLang() {
+        loadAndInsertContent(this.id);
+    }
+
     let langs_buttons = document.getElementsByClassName('lang-button');
     for (let i = 0; i < langs_buttons.length; i++) {
         langs_buttons[i].addEventListener('click', setLang);
@@ -86,37 +120,22 @@ addEventListener('load', function () {
     let theme_btn = document.getElementById('theme');
     theme_btn.addEventListener("change", setTheme);
 
-    function setLang() {
-        let langId = this.id;
-        if (langId === enLangId) {
-            loadAndInsertContent(enLangId);
-        } else if (langId === ruLangId) {
-            loadAndInsertContent(ruLangId);
-        } else {
-            loadAndInsertContent(skLangId);
-        }
-    }
-
     function setTheme() {
         let body = document.querySelector("body");
         let car_btn = document.getElementsByClassName('btn');
         let second_color = document.getElementsByClassName('second-color');
-
-        // Получить элементы img с классом "img-link"
-        let imgLinks = document.querySelectorAll(".footer-img");
-        let imgTitle = document.querySelectorAll(".title-img");
+        let page_links = document.getElementsByClassName('page-links');
 
         if (theme_btn.value == 'light') {
             body.style.background = 'white';
             body.style.color = 'black';
 
-            for (let i = 0; i < car_btn.length; i++) {
-                car_btn[i].style.color = 'black';
-            }
-            for (let i = 0; i < second_color.length; i++) {
-                second_color[i].style.color = 'black';
-            }
-            // Пройти по всем элементам img и изменить их атрибуты src
+            set_color(car_btn, 'black');
+            set_color(second_color, 'black');
+            set_color(page_links, 'black');
+
+            let imgLinks = document.querySelectorAll(".footer-img");
+            let imgTitle = document.querySelectorAll(".title-img");
             imgLinks.forEach(function(link) {
                 const fileName = link.getAttribute("src").split("/").pop();
                 const newSrc = `img/social-links/${fileName}`;
@@ -132,23 +151,11 @@ addEventListener('load', function () {
             body.style.background = 'black';
             body.style.color = 'white';
 
-            for (let i = 0; i < car_btn.length; i++) {
-                car_btn[i].style.color = 'white';
-            }
-            for (let i = 0; i < second_color.length; i++) {
-                second_color[i].style.color = 'white';
-            }
-            // Пройти по всем элементам img и изменить их атрибуты src
-            imgLinks.forEach(function(link) {
-                const fileName = link.getAttribute("src").split("/").pop();
-                const newSrc = `img/social-links/white/${fileName}`;
-                link.setAttribute("src", newSrc);
-            });
-            imgTitle.forEach(function(link) {
-                const fileName = link.getAttribute("src").split("/").pop();
-                const newSrc = `img/buttons/white/${fileName}`;
-                link.setAttribute("src", newSrc);
-            });
+            set_color(car_btn, 'white');
+            set_color(second_color, 'white');
+            set_color(page_links, 'white');
+
+            updateIconColors('white');
         }
     }
 });
